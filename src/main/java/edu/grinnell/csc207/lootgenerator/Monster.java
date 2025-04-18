1b9c5a7ea7e86087741c5a2e8a39c00284469efe
fui <fui@localhost>
2025-04-18 17:42:44 -0500

    Implemented the Monster class

---
+++ src/main/java/edu/grinnell/csc207/lootgenerator/Monster.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.grinnell.csc207.lootgenerator;
+
+/**
+ *
+ * @author fui
+ */
+public class Monster {
+    
+    private final String className;
+    private final String type;
+    private final int level;
+    private final String treasureClass;
+    
+    /**
+     * Constructs a Monster
+     *
+     * @param name     Monster name
+     * @param type          Monster type
+     * @param level         Monster level
+     * @param treasureClass Monster treasureClass
+     */
+    public Monster(String name, String type, int level, String treasureClass) {
+        this.className = name;
+        this.type = type;
+        this.level = level;
+        this.treasureClass = treasureClass;
+    }
+
+    /**
+     * returns the name
+     * 
+     * @return String
+     */
+    public String getName() {
+        return className;
+    }
+
+    /**
+     * returns the type
+     * 
+     * @return String
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * returns the treasure class
+     * 
+     * @return String
+     */
+    public String getTreasureClass() {
+        return treasureClass;
+    }
+
+    /**
+     * returns the level
+     * 
+     * @return int
+     */
+    public int getLevel() {
+        return level;
+    }
+    
+}
