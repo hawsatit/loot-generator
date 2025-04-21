bbcee84c49667896dcd5b4fedb855f22774441f7
fui <fui@fui-ubuntu>
2025-04-20 19:50:45 -0500

    fixed style error

--- src/main/java/edu/grinnell/csc207/lootgenerator/TreasureClass.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/TreasureClass.java
@@ -1,11 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package edu.grinnell.csc207.lootgenerator;
 
 /**
- *
+ * The class for the Treasure data type
+ * 
  * @author fui
  */
 public class TreasureClass {
@@ -13,56 +11,55 @@ public class TreasureClass {
     private final String item1;
     private final String item2;
     private final String item3;
-    
+
     /**
-     * Initializes the TreasureClass
+     * Initializes the TreasureClass.
      * 
      * @param name name of the Treasure Class
      * @param item1 the first item drop
      * @param item2 the second item drop
      * @param item3 the third item drop
      */
-    public TreasureClass(String name, String item1, String item2, String item3){
+    public TreasureClass(String name, String item1, String item2, String item3) {
         this.className = name;
         this.item1 = item1;
         this.item2 = item2;
         this.item3 = item3;
     }
-    
+
     /**
-     * returns the name of the TreasureClass
+     * Returns the name of the TreasureClass.
      * 
      * @return String
      */
     public String getName() {
         return className;
     }
-    
+
     /**
-     * returns the first possible drop
+     * Returns the first possible drop.
      * 
      * @return String
      */
-    public String getItem1(){
+    public String getItem1() {
         return this.item1;
     }
-    
+
     /**
-     * returns the second possible drop
+     * Returns the second possible drop.
      * 
      * @return String
      */
-    public String getItem2(){
+    public String getItem2() {
         return this.item2;
     }
-    
+
     /**
-     * returns the third possible drop
+     * Returns the third possible drop.
      * 
      * @return String
      */
-    public String getItem3(){
+    public String getItem3() {
         return this.item3;
     }
-            
 }
